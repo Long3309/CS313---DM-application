7d5c02d8edbbee2aedd0b773f418098d3ec8307c
Long3309 <kaizernguyencs@gmail.com>
2023-03-14 19:39:40 +0700

    commit all

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import streamlit as st 
-st.header("Hoang Long")
-st.markdown("### CS313 - Project")
-
-st.markdown("### CS313 - Project")
-st.markdown("### CS313 - Project")
-st.markdown("### CS313 - Project")
+import pandas as pd
+data = pd.read_csv(r"datasets\movie_metadata.csv")
+st.dataframe(data)
+code = '''
+data.isnull().sum()
+col = data["duration"]
+col = col.dropna()
+zscore = stats.zscore(col)
+res = np.where(zscore > 3.0)
+res
+'''
+st.code(code, language= "python")
